f19300cff38875edd784873915dbbfeed8081dbe
Yu Ying Li Wen <usbthekey@gmail.com>
2024-01-02 03:10:46 -0500

    job experience is now loaded

--- js/index.js
+++ js/index.js
@@ -1,70 +1,80 @@
+function init() {}
 
-
-function init()
-{
-    
+function createElement(elementName, attribute, attrName) {
+  const element = document.createElement(elementName);
+  element.setAttribute(attribute, attrName);
+  return element;
 }
 
-function createElement(elementName, attribute, attrName)
-{
-    const element = document.createElement(elementName);
-    element.setAttribute(attribute, attrName);
-    return element;   
+function createSection(attrName) {
+  return createElement("section", "class", attrName);
 }
 
-function createSection(attrName)
-{
-    return createElement("section", "class", attrName);
+function createP(attrName) {
+  return createElement("p", "class", attrName);
 }
 
-function createP(attrName)
-{
-    return createElement("p", "class", attrName);
+function createTime(attrName) {
+  return createElement("time", "class", attrName);
 }
 
-function createTime(attrName)
+function createUl(attrName)
 {
-    return createElement("time", "class", attrName);
+  return createElement("ul", "class", attrName);
 }
 
-function makeJobHTMLElement(jobData)
+function createLi(attrName)
 {
-    const sectionJob = createSection("job");
-    const sectionJobHeader = createSection("job-header");
-    const sectionJobHeaderLeftPanel = createSection("job-header-left-panel");
-    const sectionJobHeaderRightPanel = createSection("job-header-right-panel");
-    const sectionJobDescription = createSection("job-description");
+  return createElement("li", "class", attrName);
+}
 
-    const pJobTitle = createP("job-title");
-    const pJobOrganization = createP("job-title");
-    const pJobLocation = createP("job-title");
-    const pJobDate = createP("job-title");
+function makeJobHTMLElement(jobData) {
+  const sectionJob = createSection("job");
+  const sectionJobHeader = createSection("job-header");
+  const sectionJobHeaderLeftPanel = createSection("job-header-left-panel");
+  const sectionJobHeaderRightPanel = createSection("job-header-right-panel");
+  const sectionJobDescription = createSection("job-description");
 
-    const timeFrom = createTime("date-from");
-    const timeTo = createTime("date-to");
+  const pJobTitle = createP("job-title");
+  const pJobOrganization = createP("job-title");
+  const pJobLocation = createP("job-title");
+  const pJobDate = createP("job-title");
 
-    sectionJob.appendChild(sectionJobHeader);
-    sectionJobHeader.appendChild(sectionJobHeaderLeftPanel);
-    sectionJobHeader.appendChild(sectionJobHeaderRightPanel);
-    sectionJobHeader.appendChild(sectionJobDescription);
-    sectionJobHeaderLeftPanel.appendChild(pJobTitle);
-    sectionJobHeaderLeftPanel.appendChild(pJobOrganization);
-    sectionJobHeaderLeftPanel.appendChild(pJobLocation);
-    sectionJobHeaderRightPanel.appendChild(pJobDate);
-    pJobDate.appendChild(timeFrom);
-    pJobDate.appendChild(timeTo);
+  const timeFrom = createTime("date-from");
+  const timeTo = createTime("date-to");
 
-    pJobTitle.textContent = jobData["title"];
-    pJobOrganization.textContent = jobData["organization"];
-    pJobLocation.textContent = jobData["location"];
+  const jobDescList = createUl("job-description-list");
 
-    timeFrom.textContent = jobData["time-from-month"] + " " +jobData["time-from-year"];
-    timeTo.textContent = jobData["time-to-month"] + " " + jobData["time-to-year"];
+  sectionJob.appendChild(sectionJobHeader);
+  sectionJobHeader.appendChild(sectionJobHeaderLeftPanel);
+  sectionJobHeader.appendChild(sectionJobHeaderRightPanel);
+  sectionJobHeader.appendChild(sectionJobDescription);
+  sectionJobHeaderLeftPanel.appendChild(pJobTitle);
+  sectionJobHeaderLeftPanel.appendChild(pJobOrganization);
+  sectionJobHeaderLeftPanel.appendChild(pJobLocation);
+  sectionJobHeaderRightPanel.appendChild(pJobDate);
+  sectionJobDescription.appendChild(jobDescList);
 
-    let jobDate = timeFrom.textContent +  " ― " + timeFrom.textContent;
-    pJobDate.textContent = jobDate;
+  pJobTitle.textContent = jobData["title"];
+  pJobOrganization.textContent = jobData["organization"];
+  pJobLocation.textContent = jobData["location"];
 
-    return sectionJob;
+  timeFrom.textContent =
+    jobData["time-from-month"] + " " + jobData["time-from-year"];
+  timeTo.textContent = jobData["time-to-month"] + " " + jobData["time-to-year"];
+
+  pJobDate.textContent = " ― ";
+  pJobDate.prepend(timeFrom);
+  pJobDate.appendChild(timeTo);
+
+  for (const task of jobData["job-description"]) {
+
+    const taskItem = createLi("job-description-list-item");
+    taskItem.textContent = task;
+    jobDescList.appendChild(taskItem);
+  }
+
+  return sectionJob;
 }
 
 async function loadJobExperiences() {
@@ -72,12 +82,10 @@ async function loadJobExperiences() {
     response.json().then((json) => {
       const jobs = document.getElementById("jobs");
 
-      for (let job of json["job-experiences"]) {
+      for (const job of json["job-experiences"]) {
         console.log(job["title"]);
 
         jobs.appendChild(makeJobHTMLElement(job));
-
-
       }
     })
   );
